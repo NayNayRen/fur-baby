0c8a04a8534d141a43f247a1c48bb30ea8fd9147
Nate M <jeditendencies35@gmail.com>
2021-08-03 16:49:06 -0400

    reworked index.html

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@
 
 const form = document.getElementById('form');
 
-function submitForm(){
+function submitForm(e){
+  e.preventDefault();
   alert('Your information has been submitted. Thank you for joining!');
 }
 
